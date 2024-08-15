8b0e016b8fb8e0223d50f6e039a1a3059c553521
Averydx <79957750+Averydx@users.noreply.github.com>
2024-08-15 14:41:23 -0700

    new file:   Python.gitignore 	# modified:   __pycache__/models.cpython-312.pyc 	# modified:   __pycache__/particle_filter.cpython-312.pyc 	# modified:   __pycache__/pmcmc.cpython-312.pyc 	modified:   examples/pmcmc_sir.ipynb 	modified:   models.py 	modified:   notebooks/PMCMC_OU.ipynb 	modified:   notebooks/pf_variance.ipynb

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from scipy.stats import norm,nbinom
 
 '''OU Implementations'''
 @nb.njit
-def OU_model(particles, observations, t, dt, theta, rng,num_particles):
+def OU_model(particles,observations,t,dt,theta,rng,num_particles):
     particles[:,0,t] += -theta[0] * (particles[:,0,t] - theta[1]) * \
     dt + np.sqrt(2 * theta[0]) * theta[2] * rng.normal(size = (num_particles,),scale = np.sqrt(dt))
 
@@ -13,7 +13,7 @@ def OU_model(particles, observations, t, dt, theta, rng,num_particles):
     return particles,observations
 
 @nb.njit
-def OU_Obs(data_point,particle_observations, theta):
+def OU_Obs(data_point, particle_observations, theta):
     return 1/np.sqrt(2 * np.pi * theta[3]**2) * np.exp(-((data_point - particle_observations[:,0])**2)/(2 * theta[3] ** 2))
 
 
